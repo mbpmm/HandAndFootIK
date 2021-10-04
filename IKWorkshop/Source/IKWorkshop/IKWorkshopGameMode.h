702c7235876bddf754251613d066d4359abad479
mbpmm <marianomcr92@gmail.com>
2021-10-04 19:26:45 -0300

    First commit

---
+++ IKWorkshop/Source/IKWorkshop/IKWorkshopGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IKWorkshopGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AIKWorkshopGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AIKWorkshopGameMode();
+};
+
+
+
